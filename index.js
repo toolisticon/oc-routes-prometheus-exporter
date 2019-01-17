97a1b8361a10f24495ad494d9b5c27630f60b918
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-01-17 16:22:59 +0100

    feat(Prometheus): Adding monitoring endpoint via prometheus format

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
+const http = require('http');
 const request = require('request-promise-native');
 
 const log = require('./lib/logger');
 const routes = require('./lib/routes');
+const prometheus = require('./lib/prometheus');
 
 const baseUrl = 'https://http-observatory.security.mozilla.org/api/v1/analyze';
 
@@ -26,28 +28,53 @@ async function receiveScanResult (hostname) {
   };
 
   const response = await request(options);
-  log.info(response);
+  response.url = hostname;
+  prometheus.addMetric(response);
 }
-log.info('Start reading route information.');
-// read routes
-routes.list().then(routes => {
-  // reset hosts
-  hosts = [];
-  log.info('Start triggering scan.');
-  // reread hosts from route info
-  routes.forEach((route) => {
-    const hostname = route.spec.host;
-    hosts.push(hostname);
-    log.info(`Triggering scan for ${hostname}`);
-    triggerScan(hostname);
-  });
-}, log.error);
 
-// after 5 minutes read results
-setTimeout(() => {
-  log.info(`Receiving results for ${hosts.length} configured hosts.`);
-  hosts.forEach((hostname) => {
-    log.info(`Reading scan results for ${hostname}`);
-    receiveScanResult(hostname);
+async function init (hostname) {
+  log.info('Start reading route information.');
+  // read routes
+  routes.list().then(routes => {
+    // reset hosts
+    hosts = [];
+    log.info('Start triggering scan.');
+    // reread hosts from route info
+    routes.forEach((route) => {
+      const hostname = route.spec.host;
+      hosts.push(hostname);
+      log.info(`Triggering scan for ${hostname}`);
+      triggerScan(hostname);
+
+      // defer read results
+      setTimeout(() => {
+        log.info(`Receiving results for ${hosts.length} configured hosts.`);
+        hosts.forEach((hostname) => {
+          log.info(`Reading scan results for ${hostname}`);
+          receiveScanResult(hostname);
+        });
+      }, 20000);
+    });
+  }, log.error);
+}
+
+init();
+
+// start http server
+function exporter () {
+  const server = http.createServer((req, res) => {
+    switch (req.url) {
+      case '/':
+        return res.end(prometheus.renderMetrics());
+      default:
+        return res.end('404');
+    }
   });
-}, 300000);
+
+  const port = 9000; // TODO move to arg
+
+  server.listen(port);
+  log.info(`prometheus-exporter listening at ${port}`);
+}
+
+exporter();
