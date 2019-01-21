aa160f1ffb1005c49d19c279d272547cb6a8bf95
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-01-21 16:34:32 +0100

    chore(Tests): Fix failed tests

--- spec/prometheus.spec.js
+++ spec/prometheus.spec.js
@@ -45,19 +45,32 @@ describe('prometheus', () => {
     it('should convert data objects with nesting and dates', () => {
       prometheus.addMozillaMetric(validData);
       const result = prometheus.renderMetrics();
-      console.log(result);
       expect(result.indexOf('security_ssl_mozilla_observatory{algorithm_version="2",end_time="1547804767000",grade="D",hidden="false",likelihood_indicator="MEDIUM",scan_id="9783173",score="35",start_time="1547804762000",state="FINISHED",status_code="404",tests_failed="3",tests_passed="9",tests_quantity="12",url="sub.domain-sample.com",} NaN') >= 0).toBeTruthy();
+    });
+  });
+  describe('expire details', () => {
+    it('should add unknown score if invalid request', () => {
+      prometheus.addExpireMetric({});
+      const result = prometheus.renderMetrics();
+      expect(result.indexOf('security_ssl_expire_days_remaining{} NaN') >= 0).toBeTruthy();
+    });
+    it('should add additional data objects', () => {
+      prometheus.addExpireMetric({ quantile: 1 }, { key1: 'a', key2: 'b' });
+      const result = prometheus.renderMetrics();
+      expect(result.indexOf('security_ssl_expire_days_remaining{quantile="1",key1="a",key2="b",} 1.0') >= 0).toBeTruthy();
     });
   });
   describe('ssl details', () => {
     it('should add unknown score if invalid request', () => {
-      prometheus.addExpireMetric({});
+      prometheus.addDetailsMetric({});
       const result = prometheus.renderMetrics();
+      console.log(result);
       expect(result.indexOf('security_ssl_details{} NaN') >= 0).toBeTruthy();
     });
     it('should add additional data objects', () => {
-      prometheus.addExpireMetric({ quantile: 200 }, { key1: 'a', key2: 'b' });
+      prometheus.addDetailsMetric({ quantile: 200 }, { key1: 'a', key2: 'b' });
       const result = prometheus.renderMetrics();
+      console.log(result);
       expect(result.indexOf('security_ssl_details{quantile="200",key1="a",key2="b",} 200.0') >= 0).toBeTruthy();
     });
   });
