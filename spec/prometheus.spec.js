dd696a8bb1367c0cb5cccd24fa440887563469a4
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-01-21 15:26:02 +0100

    fix(Prometheus): Corrected parsing.
    Skipping not need properties.

--- spec/prometheus.spec.js
+++ spec/prometheus.spec.js
@@ -37,9 +37,15 @@ describe('prometheus', () => {
       const result = prometheus.renderMetrics();
       expect(result.indexOf('security_ssl_mozilla_observatory{quantile="1",key1="a",key2="b",} 1.0') >= 0).toBeTruthy();
     });
+    it('should add additional data objects', () => {
+      prometheus.addMozillaMetric({ quantile: 1 }, { key1: 'a', key2: 'b' });
+      const result = prometheus.renderMetrics();
+      expect(result.indexOf('security_ssl_mozilla_observatory{quantile="1",key1="a",key2="b",} 1.0') >= 0).toBeTruthy();
+    });
     it('should convert data objects with nesting and dates', () => {
       prometheus.addMozillaMetric(validData);
       const result = prometheus.renderMetrics();
+      console.log(result);
       expect(result.indexOf('security_ssl_mozilla_observatory{algorithm_version="2",end_time="1547804767000",grade="D",hidden="false",likelihood_indicator="MEDIUM",scan_id="9783173",score="35",start_time="1547804762000",state="FINISHED",status_code="404",tests_failed="3",tests_passed="9",tests_quantity="12",url="sub.domain-sample.com",} NaN') >= 0).toBeTruthy();
     });
   });
